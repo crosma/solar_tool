faca20905efc13bfac66d16296a56edd2ae218d4
Matt Crossley <vbaspcppguy@gmail.com>
2016-08-22 15:20:31 -0700

    Why wont you work!?

--- git-push-pages.sh
+++ git-push-pages.sh
@@ -3,8 +3,9 @@
 
 ng build #--prod
 
-git add dist/
+git add -f dist/
 git commit -m "Updating gh-pages"
 #git push
 git subtree push --prefix dist/ origin gh-pages
-git reset --soft HEAD^
+#git reset --soft HEAD^
+git rm -r --cached dist
