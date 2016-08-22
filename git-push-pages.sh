f5178ea25f73564fb791af1fd582f7c4c6ba2f97
Matt Crossley <vbaspcppguy@gmail.com>
2016-08-22 16:05:43 -0700

    Cleanup

--- git-push-pages.sh
+++ git-push-pages.sh
@@ -4,8 +4,14 @@
 ng build #--prod
 
 git add dist
-git commit -m "Updating gh-pages"
+git commit -m "Updating gh-pages (temp message)"
+
+git branch -D gh-pages || true
+git subtree split --prefix dist -b gh-pages # create a local gh-pages branch containing the splitted output folder
+git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git branch -D gh-pages # delete the local gh-pages because you will need it: ref
+
 #git push
-git subtree push --prefix dist origin gh-pages
-#git reset --soft HEAD^
+#git subtree push --prefix dist origin gh-pages
+git reset --soft HEAD^
 #git rm -r --cached dist
