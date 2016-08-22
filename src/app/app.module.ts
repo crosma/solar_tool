d8143e009884d66e8abf2179e1c5ee920f80377b
Matt Crossley <vbaspcppguy@gmail.com>
2016-08-21 20:33:21 -0700

    Working on git pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,7 @@ import { AppComponent } from './app.component';
 import { SolarComponent } from './solar/solar.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
 import { MainChartComponent } from './solar/main-chart/main-chart.component';
-import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
@@ -19,9 +17,7 @@ import { TodoComponent } from './todo/todo.component';
     SolarComponent,
     AboutComponent,
     PageNotFoundComponent,
-    HomeComponent,
     MainChartComponent,
-    TodoComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,26 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 
 //import components
-import { HomeComponent }  from './home/home.component';
 import { SolarComponent }  from './solar/solar.component';
 import { AboutComponent }  from './about/about.component';
-import { TodoComponent }  from './todo/todo.component';
 import { PageNotFoundComponent }  from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: SolarComponent
-  },
-
-  {
-    path: 'solar',
-    component: SolarComponent
-  },
-
-  {
-    path: 'todo',
-    component: TodoComponent
   },
 
   {
