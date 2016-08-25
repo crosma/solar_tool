9a635d49be641809cf7df9add48e481dcaa83bc1
Matt Crossley <vbaspcppguy@gmail.com>
2016-08-25 00:42:13 -0700

    A little cleanup.

--- git-push-pages.sh
+++ git-push-pages.sh
@@ -10,8 +10,5 @@ git branch -D gh-pages || true
 git subtree split --prefix dist -b gh-pages # create a local gh-pages branch containing the splitted output folder
 git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
 git branch -D gh-pages # delete the local gh-pages because you will need it: ref
-
-#git push
-#git subtree push --prefix dist origin gh-pages
 git reset --soft HEAD^
 git rm -r --cached dist
